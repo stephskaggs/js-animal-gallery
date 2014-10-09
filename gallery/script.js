1b08f5327901d567ec8a5603601e5e270b6dd369
stephskaggs <snskaggs@gmail.com>
2014-10-08 20:24:35 -0500

    made the toggle work

--- gallery/script.js
+++ gallery/script.js
@@ -1,1 +1,37 @@
 // Add your code here
+$(document).ready(function() {
+
+$ ('.filter-button').on('click' , function (e){
+	var species = $(this).attr("id");
+
+	if ($(this).hasClass('active')) {
+		hideAnimals(species);
+	}else{
+		showAnimals(species);
+	}
+	$('#' + species).toggleClass('active');
+});
+
+
+// $ ('.filter-button').on('click' , function (species){
+// 	species = $(this).attr("id");
+// 	$('.' + species).hide();
+// 	$(this).removeClass('active');
+// });
+
+function hideAnimals(species){
+	$('.' + species).hide();
+	// $('#' + species).removeClass('active');
+}
+
+function showAnimals(species){
+	$('.' + species).show();
+	// $('#' + species).addClass('active');
+}
+
+});
+
+// Make it so the button is no longer green
+
+
+// reveal the hidden buttons
